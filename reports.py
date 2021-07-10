43d6b2316dd7f9c6556344f096e0db588553c49a
Alex Jacobs <71242672+Axle-git@users.noreply.github.com>
2021-07-10 05:40:16 -0600

    Update reports.py

--- reports.py
+++ reports.py
@@ -1,6 +1,9 @@
 """
 WARNING:    This script will ONLY WORK ON THE CURRENT FRONT DESK COMPUTER AT SORREL RIVER RANCH (7/8/2021).
             In the event that we recieve new computers (god willing), there are instructions on how to change the pixel counts in the XYcoordinates.py file. 
+            All files are saved in Ajacobs account
+
+WARNING:    RK save to PDF screen must be full screen
 
 This script does the following steps in order:
     1)  opens RoomKeyPMS and logs in
@@ -11,24 +14,23 @@ This script does the following steps in order:
 
 from printReports import *
 
-import os
-import sys
-import time
-import pyautogui as ag
+if __name__ == '__main__':
 
-####    Make Report Folder      ####
+    ####    Make Report Folder      ####
+    os.mkdir(path)
+    time.sleep(1)
 
-####    open roomkey            ####
-ag.press('win')
-ag.write('Roomkey',interval = .1)
-ag.press('enter')
-time.sleep(5)   #wait for roomkey to startup
+    ####    open roomkey            ####
+    ag.press('win')
+    ag.write('Roomkey',interval = .1)
+    ag.press('enter')
+    time.sleep(5)   #wait for roomkey to startup
 
-####    login                   ####
-ag.write('alexjacobs', interval = .1)   #username
-ag.press('tab')
-ag.write('LaceFace11', interval = .1)   #password
-ag.press('enter')
-time.sleep(20)  #wait for roomkey
+    ####    login                   ####
+    ag.write('alexjacobs', interval = .1)   #username
+    ag.press('tab')
+    ag.write('LaceFace11', interval = .1)   #password
+    ag.press('enter')
+    time.sleep(25)  #wait for roomkey
 
-printInHouse()
+    printInHouse()
