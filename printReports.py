84ac65ec7e800a0bf7821b94be9688b56d432f98
Alex Jacobs <71242672+Axle-git@users.noreply.github.com>
2021-07-10 05:57:34 -0600

    Update printReports.py

--- printReports.py
+++ printReports.py
@@ -22,32 +22,6 @@ def printInHouse():
     time.sleep(.1)
     ag.click(IHbyRoomNumberX,IHbyRoomNumberY)
     time.sleep(5)
-    
-    #   save report
-    #   WARNING: save screen must be full screen
-    ag.click(printerSetupX,printerSetupY)
-    time.sleep(1)
-    ag.click(printerNameX,printerNameY)
-    time.sleep(.1)
-    ag.write(printToPDF, interval = .1)
-    ag.press('enter')
-    time.sleep(.1)
-    ag.press('enter')
-    ag.click(printerX,printerY)
-    time.sleep(2)
-    ag.click(folderPathX,folderPathY)   #navigate to folder
-    time.sleep(.1)
-    ag.write('C:\\users\\ajacobs\\reports\\' + currDate, interval= .1)
-    ag.press('enter')
-    time.sleep(.1)
-    ag.click(folderPathX,folderPathY)
-    time.sleep(.1)
-    for x in range(7):  #get to file name
-        ag.press('tab')
-        time.sleep(.1)
-    ag.write("InHouse Report " + currDate, interval = .1)
-    ag.press('enter')
-    time.sleep(.1)
 
     #   print reports
     ag.click(printerSetupX,printerSetupY)
@@ -56,11 +30,6 @@ def printInHouse():
     time.sleep(.1)
     ag.write(printerName, interval = .1)
     ag.press('enter')
-    time.sleep(.1)
-    for x in range(5):
-        ag.press('tab')
-        time.sleep(.1)
-    ag.press('3')
     time.sleep(.1)
     ag.press('enter')
     time.sleep(1)
